22e619fc8702e98521190640a00291e17f4dfffa
Vanesa Reinerth <vanesa@getbowtied.com>
2019-01-22 12:33:50 +0200

    posts grid neutrality

--- includes/gbt-blocks/posts_grid/block.php
+++ includes/gbt-blocks/posts_grid/block.php
@@ -57,75 +57,50 @@ if ( ! function_exists( 'gbt_18_th_render_frontend_posts_grid' ) ) {
 	    
 	    $recentPosts = get_posts( $args );
 
-	    $post_columns = '4';
-
-	    switch( $columns ) {
-	    	case '1':
-	    		$post_columns = '12';
-	    		break;
-	    	case '2':
-	    		$post_columns = '6';
-	    		break;
-	    	case '3':
-	    		$post_columns = '4';
-	    		break;
-	    	case '4':
-	    		$post_columns = '3';
-	    		break;
-	    	default:
-	    		$post_columns = '4';
-	    		break;
-	    }
-
 		ob_start();
 		        
 	    if ( !empty($recentPosts) ) : ?>
 
-		    <div class="gbt_18_th_posts_grid">
+		    <div class="gbt_18_th_posts_grid align<?php echo $align; ?>">
 
-				<div class="gbt_shortcode_blog_posts <?php echo $align; ?>">
-					<div class="row">
+				<div class="gbt_18_th_blog_posts columns-<?php echo $columns; ?>">
 				                    
-				        <?php foreach($recentPosts as $post) : ?>
-				    
-				        	<div class="small-12 medium-6 large-<?php echo $post_columns; ?> columns">
+			        <?php foreach($recentPosts as $post) : ?>
+			    
+						<div class="gbt_18_tr_posts_grid_item">
 
-								<div class="gbt_shortcode_blog_post">
+							<?php if ( has_post_thumbnail($post->ID) ) : 
+								$image_id = get_post_thumbnail_id($post->ID);
+								$image_url = wp_get_attachment_image_src($image_id,'large', true);
+							?>
+								<a href="<?php echo get_post_permalink($post->ID); ?>">
+									<span class="gbt_18_th_posts_grid_img gbt_18_th_posts_grid_with_img" 
+										style="background-image: url(<?php echo esc_url($image_url[0]); ?> );">
+									</span>
+								</a>
+							<?php endif; ?>
 
-									<?php if ( has_post_thumbnail($post->ID) ) : 
-										$image_id = get_post_thumbnail_id($post->ID);
-										$image_url = wp_get_attachment_image_src($image_id,'large', true);
-									?>
-										<a href="<?php echo get_post_permalink($post->ID); ?>">
-											<span class="gbt_18_th_posts_grid_img gbt_18_th_posts_grid_with_img" 
-												style="background-image: url(<?php echo esc_url($image_url[0]); ?> );">
-											</span>
-										</a>
-									<?php endif; ?>
-
-									<div class="gbt_shortcode_blog_posts_content">
-										<div class="gbt_shortcode_blog_posts_meta">
-											<a href="<?php echo get_post_permalink($post->ID); ?>" rel="bookmark">
-												<time class="entry-date published" datetime="<?php echo get_the_date( DATE_W3C, $post->ID ); ?>">
-													<?php echo get_the_date( '', $post->ID ); ?>
-												</time>
-											</a>
-										</div>
-										<h4 class="gbt_shortcode_blog_posts_title site-secondary-font">
-											<a href="<?php echo get_post_permalink($post->ID); ?>">
-												<?php echo $post->post_title; ?>
-											</a>
-										</h4>
-									</div>
-
+							<div class="gbt_18_th_blog_post_content">
+								<div class="gbt_18_th_blog_post_meta">
+									<a href="<?php echo get_post_permalink($post->ID); ?>" rel="bookmark">
+										<time class="entry-date published" datetime="<?php echo get_the_date( DATE_W3C, $post->ID ); ?>">
+											<?php echo get_the_date( '', $post->ID ); ?>
+										</time>
+									</a>
 								</div>
-
+								<h4 class="gbt_18_th_blog_post_title site-secondary-font">
+									<a href="<?php echo get_post_permalink($post->ID); ?>">
+										<?php echo $post->post_title; ?>
+									</a>
+								</h4>
 							</div>
 
-				        <?php endforeach; // end of the loop. ?>
+						</div>
 
-		        	</div>
+			        <?php endforeach; // end of the loop. ?>
+
 		        </div>
+		        
 		    </div>
 
 	    <?php
