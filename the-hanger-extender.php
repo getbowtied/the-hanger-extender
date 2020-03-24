fef8d393420977d4536fdd826c7f866e68819acd
Vanesa Reinerth <vanesa@getbowtied.com>
2020-03-24 11:32:49 +0200

    v.1.6.4

--- the-hanger-extender.php
+++ the-hanger-extender.php
@@ -3,11 +3,11 @@
 	 * Plugin Name: The Hanger Extender
 	 * Plugin URI: https://thehanger.wp-theme.design/
 	 * Description: Extends the functionality of The Hanger with theme specific shortcodes and page builder elements.
-	 * Version: 1.6.3
+	 * Version: 1.6.4
 	 * Author: GetBowtied
 	 * Author URI: https://getbowtied.com
 	 * Requires at least: 5.0
-	 * Tested up to: 5.3.1
+	 * Tested up to: 5.3.2
 	 *
 	 * @package  The Hanger Extender
 	 * @author GetBowtied
