9125f3f31b70a5c56e296e91b269d2d390596665
Vanesa Reinerth <vanesa@getbowtied.com>
2020-12-17 12:41:45 +0200

    v.1.7.0

--- includes/gbt-blocks/slider/assets/js/slider.js
+++ includes/gbt-blocks/slider/assets/js/slider.js
@@ -2,13 +2,20 @@ jQuery(function($) {
 
 	"use strict";
 
+	function th_generate_slider_unique_ID() {
+		return Math.round(new Date().getTime() + (Math.random() * 100));
+	}
+
 	$('.gbt_18_th_slider_container').each(function() {
+
+		var data_id = th_generate_slider_unique_ID();
+		$(this).addClass( 'swiper-' + data_id );
 
 		var autoplay = $(this).find('.swiper-slide').length > 1 ? { delay: 4000 } : false;
 		var loop = $(this).find('.swiper-slide').length > 1 ? true : false;
-		var pagination = $(this).find('.swiper-slide').length > 1 ? { el: '.gbt_18_th_slider_container .gbt_18_th_slider_pagination', clickable: true } : false;
+		var pagination = $(this).find('.swiper-slide').length > 1 ? { el: '.swiper-' + data_id + ' .gbt_18_th_slider_pagination', clickable: true } : false;
 
-		var mySwiper = new Swiper( '.gbt_18_th_slider_container', {
+		var mySwiper = new Swiper( '.swiper-' + data_id, {
 
 			// Optional parameters
 		    direction: 'horizontal',
