96dbea21a2fd1e8a04991c30d560e5f14315c2f4
Romi H. <romi@getbowtied.com>
2025-11-11 11:24:45 +0200

    3.9

--- the-hanger-extender.php
+++ the-hanger-extender.php
@@ -3,7 +3,7 @@
  * Plugin Name: The Hanger Extender
  * Plugin URI: https://thehanger.getbowtied.com
  * Description: Extends the functionality of The Hanger with theme specific shortcodes and page builder elements.
- * Version: 3.8
+ * Version: 3.9
  * Author: Get Bowtied
  * Author URI: https://getbowtied.com
  * Requires at least: 6.0
