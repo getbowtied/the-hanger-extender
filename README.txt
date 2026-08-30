c17ebd84b425b9d20f757592dfa20ce55a6bae8f
Romi H. <romi@getbowtied.com>
2026-08-30 18:57:36 +0300

    10.0.1

--- README.txt
+++ README.txt
@@ -3,7 +3,7 @@ Contributors: getbowtied
 Tags: gutenberg, blocks
 Requires at least: 6.0
 Tested up to: 7.1
-Stable tag: 10.0
+Stable tag: 10.0.1
 Requires PHP: 7.4.1
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
@@ -35,6 +35,9 @@ Companion plugin for the **The Hanger** theme. Extends the functionality by addi
 - Adds header image and icon options for WooCommerce product categories
 
 == Changelog ==
+
+= 10.0.1 =
+- Add option to enable theme auto-updates from the update notice
 
 = 10.0 =
 - Add fallback theme updater for older theme versions
--- core/theme-updater/class-gbt-extender-theme-updater.php
+++ core/theme-updater/class-gbt-extender-theme-updater.php
@@ -237,17 +237,17 @@ if ( ! class_exists( 'GBT_Extender_Theme_Updater' ) ) {
 						<?php if ( ! $auto_on ) : ?>
 							<span
 								class="gbt-extender-auto-update-wrap"
-								data-enabling-text="<?php echo esc_attr__( 'Enabling auto-updates…', 'the-retailer-extender' ); ?>"
-								data-success-text="<?php echo esc_attr__( 'Auto-updates are enabled.', 'the-retailer-extender' ); ?>"
+								data-enabling-text="<?php echo esc_attr__( 'Enabling auto-updates…', 'the-hanger-extender' ); ?>"
+								data-success-text="<?php echo esc_attr__( 'Auto-updates are enabled.', 'the-hanger-extender' ); ?>"
 							>
 								<?php
-								echo esc_html__( 'You may also', 'the-retailer-extender' ) . ' ';
+								echo esc_html__( 'You may also', 'the-hanger-extender' ) . ' ';
 								printf(
 									'<a href="#" class="gbt-extender-enable-auto-updates" data-theme="%1$s"><strong>%2$s</strong></a>',
 									esc_attr( $slug ),
-									esc_html__( 'enable automatic updates', 'the-retailer-extender' )
+									esc_html__( 'enable automatic updates', 'the-hanger-extender' )
 								);
-								echo ' ' . esc_html__( "so you won't be bothered again.", 'the-retailer-extender' );
+								echo ' ' . esc_html__( "so you won't be bothered again.", 'the-hanger-extender' );
 								?>
 							</span>
 						<?php endif; ?>
